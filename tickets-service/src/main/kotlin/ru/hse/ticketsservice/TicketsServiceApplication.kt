d627662dad01a6ff2ba58e93b87d07894a1df3e6
Kirill Lysin <kslysin_1@edu.hse.ru>
2024-06-07 19:11:07 +0300

    Init tickets service module

---
+++ tickets-service/src/main/kotlin/ru/hse/ticketsservice/TicketsServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.hse.ticketsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketsServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketsServiceApplication>(*args)
+}
